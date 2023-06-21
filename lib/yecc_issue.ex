8f9b9fdb2b7a2138aff3bd6c110dd4ecf8457aa6
Eugene Pirogov <iamexile@gmail.com>
2023-06-21 14:58:33 +0200

    mix new yecc_issue

---
+++ lib/yecc_issue.ex
@@ -0,0 +1,18 @@
+defmodule YeccIssue do
+  @moduledoc """
+  Documentation for `YeccIssue`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YeccIssue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/yecc_issue_test.exs
@@ -0,0 +1,8 @@
+defmodule YeccIssueTest do
+  use ExUnit.Case
+  doctest YeccIssue
+
+  test "greets the world" do
+    assert YeccIssue.hello() == :world
+  end
+end
